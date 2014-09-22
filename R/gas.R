37482a87cd2ff97e25a2fa7c77e5c4c49ee29de8
= <=>
2014-09-22 23:41:48 +0100

    added data and drag coefficents

--- R/gas.R
+++ R/gas.R
@@ -1,7 +1,8 @@
-#' Wanninkhov et al, 2009 Gas transfer velocity
+#' Wanninkhov et al (2009) Gas transfer velocity
 #'
-#' Implements the gas transfer velocity parametrisation of Wanninkhov et al, 2009.
+#' Implements the gas transfer velocity parametrisation of Wanninkhov et al (2009).
 #'
+#' @details TODO
 #' @param compound character string of compound of interest
 #' @param T vector of temperature in degrees Centigrade
 #' @param u vector of wind speed in meters per second at 10 meters height
@@ -10,26 +11,29 @@
 #' @keywords kw gas exchange
 #' @references Wanninkhof, R., Asher, W. E., Ho, D. T., Sweeney, C., & McGillis, W. R. (2009).  'Advances in Quantifying Air-Sea Gas Exchange and Environmental Forcing*. Annual Review of Marine Science, 1(1), 213–244. doi:10.1146/annurev.marine.010908.163742
 #' @examples
-#' Wann09kw('O2', 10, 7, 35)  # gas transfer velocity for oxygen
-Wann09kw <- function(compound,T,u,S,normalize=0,schmidt_formulation=0){
+#' Wann09Kw('O2', 10, 7, 35)  # gas transfer velocity for oxygen
+Wann09Kw <- function(compound, T, u, S, normalize=0, schmidt_formulation=0){
     # Kw parametrisation of Wanninkhof2009
-    # output in meter per second
-    if (normalize!=0) schmidt_number<-normalize else
-        schmidt_number<-schmidt_out(compound,T,S,schmidt_formulation)
-    (3 + (0.1 * u) + (0.064 * u^2) + (0.011 * u^3))*((schmidt_number/660)^(-0.5))/(100*3600)
+    # output in meters per second
+    if (normalize!=0) schmidt_number <- normalize else
+        schmidt_number <- schmidt_out(compound, T, S, schmidt_formulation)
+    (3 + (0.1 * u) + (0.064 * u^2) + (0.011 * u^3)) * ((schmidt_number / 660)^(-0.5)) / (100 * 3600)
 }
 
-#' Woolf and Thorpe, 1991 equilibrium bubble supersaturation
+#' Woolf and Thorpe (1991) equilibrium bubble supersaturation
 #'
-#' Implements the bubble supersaturation parametrisation of Woolf and Thorpe, 1991
+#' Implements the bubble supersaturation parametrisation of Woolf and Thorpe (1991)
+#' Note: parametrisation is only applicable for Oxygen (O2), Nitrogen (N2), Argon (Ar) and Carbon Dioxide (CO2).
 #'
+#' @details TODO
+#' @param compound character string of compound of interest ('O2' ,'N2', 'Ar', 'CO2')
 #' @param u vector of wind speed in meters per second at 10 meters height
 #' @return vector of equilibrium bubble supersaturation effects (no units)
 #' @keywords kw gas exchange bubble
 #' @references Woolf, D. K., & Thorpe, S. A. (1991). Bubbles and the air-sea exchange of gases in near-saturation conditions. Journal of Marine Research, 49(3), 435–466. doi:10.1357/002224091784995765
 #' @examples
-#' Bwoolf('Ar', 7)  # bubble supersaturation effect for Argon at 7 m/s winds
-Bwoolf <- function(compound = 'O2', u){
+#' WoolfB('Ar', 7)  # bubble supersaturation effect for Argon at 7 m/s winds
+WoolfB <- function(compound = 'O2', u){
     # bubble saturation effects from Woolf1991
     # 9m/s/ = 1% supersat
     U = NA
@@ -40,4 +44,36 @@ Bwoolf <- function(compound = 'O2', u){
     B = 0.01 * (u / U)^2
     # B is a scaling term and has no units
     return(B)
+}
+
+#' Smith (1980) Drag Coefficient
+#'
+#' Calculates the drag coefficent of Smith (1980)
+#' @details TODO
+#' @param u vector of wind speed in meters per second at 10 meters height
+#' @return vector of drag coefficent
+#' @references Smith, S. D. (1980). Wind stress and heat flux over the ocean in gale force winds. J Phys. Oceanogr. 10, 709-726.
+#' @keywords drag coefficent
+SmithCD <- function(u){
+    1e-4*(6.1+0.63*u)
+}
+
+#' Saturated Large and Pond (1981) Drag Coefficent
+#'
+#' Calculates the drag coefficent of Large and Pond with the high wind saturation as per Sullivan et al, (2012)
+#' @details TODO
+#' @param u vector of wind speed in meters per second at 10 meters height
+#' @return vector of drag coefficent
+#' @references Sullivan, P. P., Romero, L., McWilliams, J. C., & Melville, W. K. (2012). Transient Evolution of Langmuir Turbulence in Ocean Boundary Layers Driven by Hurricane Winds and Waves. Journal of Physical Oceanography, 42(11), 1959–1980. doi:10.1175/JPO-D-12-025.1
+#' @keywords drag coefficent
+LargePondCD <- function(u){
+    # returns drag coefficent as per Large & Pond, 1981
+    # with high wind saturation as per Sullivan et al, 2012
+    cd <- function(x){
+               if(is.na(x)){return(NA)}
+               if(x < 11){return(0.0012)}
+               if(x > 20){return(1.8 * 10^-3)}
+               else{return((0.49 + 0.065 * x) * 10^-3)}
+    }
+    sapply(u, cd)
 }
---
+++ R/viscDen.R
@@ -0,0 +1,72 @@
+####################################################
+###       Viscosity and density of water         ###
+####################################################
+
+n_0 <- function(T){
+    #Calculate the viscosity of pure water in cP (mPa s) according to LaLiberte 2007 (doi:10.1021/je0604075). Requires T in celcius
+        (T+246)/(137.37+(5.2842*T)+(0.05594*(T^2)))
+}
+
+
+
+n_sw <- function(T,S,return_relative_viscotiy=0){
+    #calculate the dynamic viscosity of seawater in cP using the viscosity model / mixing rule approach of LaLiberte 2007
+    #to return relative viscosity (i.e. viscosity scaling factor) for comparison with other studies, pass n_sw a final argument of "1"
+    #after T and S e.g. n_sw(10,35,1)
+        #read in a data file containing the mass fraction of each constituent solute in seawater per salinity unit in per mil and the coefficients determined by LaLiberte 2007 for each salt
+        sw_cmf<-read.table("sw_constituent_mass_fractions.dat", header = TRUE, row.names = 1)
+        #w_i_ln_n_i_tot is the sum of the product of the mass fraction and the natural log of the viscosities contributed by each solute individually (see LaLiberte 2007, equation 8)
+        w_i_ln_n_i_tot <-0
+        #sum up the mass fractions to get water mass fraction
+        w_i_tot<-0
+        for (salt in row.names(sw_cmf)){
+            w_i <- sw_cmf[salt,"massfraction"]*S/1000
+            w_i_tot<-w_i_tot + w_i
+        }
+        for (salt in row.names(sw_cmf)){
+            w_i <- sw_cmf[salt,"massfraction"]*S/1000
+            v1 <- sw_cmf[salt,"v1"]
+            v2 <- sw_cmf[salt,"v2"]
+            v3 <- sw_cmf[salt,"v3"]
+            v4 <- sw_cmf[salt,"v4"]
+            v5 <- sw_cmf[salt,"v5"]
+            v6 <- sw_cmf[salt,"v6"]
+            #wi_tot is used here as eq 12 in LaLiberte requires (1-w_w), which is equivalent. Using this term seems initially counterintuitive - one might expect to use w_i here for each solute individually. However,  "at any given solute concentration, the solute viscosity increases with the total solute content" so 1-w_w (or w_i_tot) is the correct term - pers. comm. from Marc LaLiberte
+            n_i<-(exp(((v1*w_i_tot^v2)+v3)/((v4*T) + 1)))/((v5*(w_i_tot^v6))+1)
+            w_i_ln_n_i_tot <- w_i_ln_n_i_tot + (w_i*log(n_i))
+        }
+        ln_n_m<- (1-w_i_tot)*log(n_0(T))+w_i_ln_n_i_tot
+        if (return_relative_viscotiy==0) exp(ln_n_m) else
+            (exp(ln_n_m)/n_0(T))
+}
+
+n_sw_hardy <- function(T,n_zero=1){
+    #Non-salinity dependent Hardy  1953 method for calculating viscosity - not used in calculations, purely for comparison with Laliberte method.
+    # η = η0 * K/[1+0.03338.T + 0.00018325.T2]
+    # use n_zero = 1 to use Laliberte n_0 or n_zero=0 for Hardy n_0
+        if (n_zero==0) n0<- 1.787 else
+            n0 <- n_0(T)
+        (n0*1.052/(1+(0.03338*T)+(0.00018325*(T^2))))
+}
+
+p_sw <- function(T,S){
+    ## Calculate the density of seawater in kg/m3 according to millero and Poisson (1981)
+        #coefficients for millero calc
+        A <- 0.824493-(0.0040899*T)+(0.000076438*(T^2))-(0.00000082467*(T^3))+(0.0000000053875*(T^4))
+        B <- -0.00572466+(0.00010277*T)-(0.0000016546*(T^2))
+        C <- 0.00048314
+        # density of pure water
+        p_o <- 999.842594+(0.06793952*T)-(0.00909529*(T^2))+(0.0001001685*(T^3))-(0.000001120083*(T^4))+(0.000000006536332*(T^5))
+        # return salinity-and-temperature-dependent density of seawater (at atmospheric pressure)
+        (p_o+(A*S)+(B*(S^(3/2)))+(C*S))
+} 
+
+v_sw <- function(T,S) {
+    #calculate kinmatic viscosity of seawater in cm/s for Schmidt number calculation
+        # dynamic viscosity - convert to S.I. (Kg/m/s)
+        n = n_sw(T,S)/1000
+        # density already in S.I.
+        p = p_sw(T,S)
+        #multiply by 10000 to go from m2/s to cm2/s
+        10000*n/p
+}
