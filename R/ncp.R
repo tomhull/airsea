0b4662939b705d326df266e44afd0deb553bb018
Tom Hull <betascoo8@gmail.com>
2015-03-23 13:15:18 +0000

    used for loop to sort of vectorise ncp calc

--- R/ncp.R
+++ R/ncp.R
@@ -37,53 +37,30 @@ transform_data <- function(x){
 #'
 #' calculate NCP based on O2 observations
 #'
-#' @details TODO  use x$ncp = apply(x, 1, O2NCP)
+#' @details TODO
 #' @param dat data frame matching the format outlined in XXX
 #' @param entrainment if True (default) calculate NCP with entrainment, Cb0 and Cb1 must be supplied
 #' @param kw.method character string passed to kw, default is 'WA09'
 #' @return a vector of NCP in uMol/l
 #' @export
-
-
-applyer <- function(dat){
-    apply(dat, 1, O2NCP)
-}
-
 O2NCP <- function(dat, entrainment = T, kw_method = 'WA09'){
-    ti = dat['timePeriod']
-    dhdt = 0
-
-    # variable interpolators
-        mld = approxfun(c(0, ti),c(dat['h0'], dat['h1']))
-        temp = approxfun(c(0, ti), c(dat['T0'], dat['T1']))
-        sal = approxfun(c(0, ti), c(dat['S0'], dat['S1']))
-        ws = approxfun(c(0, ti), c(dat['u0'], dat['u1']))
-        Cb = approxfun(c(0, ti),c(dat['Cb0'], dat['Cb1']))
-
+    #subfunctions
     Csat.t <- function(tx){
         # calculate oxygen saturation at tx
         return(Csat(temp(tx), sal(tx)))
     }
-
     rtx <- function(tx){
         # returns r at tx (any point between t0 and t1)
         kw('O2', temp(tx), ws(tx), sal(tx), kw_method) / mld(tx) + dhdt
     }
-
     Rt <- function(tx){
         # vectorised, integrates rtx up to suppled time
         return(sapply(tx, function(y) integrate(rtx, lower = 0, upper = y)$value))
     }
-
     Q2.f <- function(tx){
         # exponential of integrated r at supplied time
         return(exp(Rt(tx)))
     }
-        # integrate exponential of integrated r at t1
-    Q2 = integrate(Q2.f, lower = 0, upper = ti)$value
-
-        # G
-
     Gtx <- function(tx, kw.method){
             # kw at tx
         kwx = kw('O2', temp(tx), ws(tx), sal(tx), method = kw_method)
@@ -91,19 +68,30 @@ O2NCP <- function(dat, entrainment = T, kw_method = 'WA09'){
         Gx = (kwx / mld(tx)) * Csat.t(tx) * (1 + bubbleSat('O2', ws(tx))) * 1 + (dhdt * Cb(tx))
         return(Gx)
     }
-
     Q1.f <- function(tx){
         Gtx(tx) * exp(Rt(tx))
     }
 
-    Q1 = integrate(Q1.f, lower = 0, upper = ti)$value
+    ncp = vector()
+    for(i in 1:nrow(dat)){
+    # variable interpolators, can't be vectorised
+    ti = dat[i,]$timePeriod
+    dhdt = 0
+        mld  = approxfun(c(0, ti), c(dat[i,]$h0, dat[i,]$h1))
+        temp = approxfun(c(0, ti), c(dat[i,]$T0, dat[i,]$T1))
+        sal  = approxfun(c(0, ti), c(dat[i,]$S0, dat[i,]$S1))
+        ws   = approxfun(c(0, ti), c(dat[i,]$u0, dat[i,]$u1))
+        Cb   = approxfun(c(0, ti), c(dat[i,]$Cb0, dat[i,]$Cb1))
 
-    print(c(Q1, Q2, dat['C0'], dat['C1'], exp(Rt(ti)), Gtx(ti)))
+    Q2 = integrate(Q2.f, lower = 0, upper = ti)$value
+    Q1 = integrate(Q1.f, lower = 0, upper = ti)$value
 
         # calculate NCP (J)
 
-    J = (dat['C1'][1] * exp(Rt(ti)) - dat['C0'][1] - Q1) / Q2
-    return(J * ti) # return as uMol per supplied time
+    J = (dat[i,]$C1 * exp(Rt(ti)) - dat[i,]$C0 - Q1) / Q2
+    ncp = c(ncp, J * ti) # return as uMol per supplied time
+    }
+    return(ncp)
 }
 
 
