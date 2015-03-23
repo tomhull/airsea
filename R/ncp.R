e3ac9990178ed64d1b8f6f05c6ec635b581e0f5a
Tom Hull <betascoo8@gmail.com>
2015-03-23 22:01:48 +0000

    removed transform_data until I decide what to do with sensitivity stuff

--- R/ncp.R
+++ R/ncp.R
@@ -1,38 +1,6 @@
 # functions for oxygen based NCP calculations
 
 
-#' timeseries translator
-#'
-#' divides up timeseries for NCP calculation
-#'
-#' @details TODO
-#' @param x a data frame matching the format outlined in XXX
-#' @return data.frame in the format expected by O2NCP()
-#' @export
-transform_data <- function(x){
-    # transforms timeseries data to inital and post variables in wide format, i.e. like a lhs
-    dat = data.frame(dateTime = x$dateTime, T0 = x$T, S0 = x$S, C0 = x$C, Cb0 = x$Cb, u0 = x$u, h0 = x$h, Pslp0 = x$Pslp)
-    dat$T1 = dat$T0 + c(diff(dat$T0), 0)
-    dat$S1 = dat$S0 + c(diff(dat$S0), 0)
-    dat$Cb1 = dat$Cb0 + c(diff(dat$Cb0), 0)
-    dat$u1 = dat$u0 + c(diff(dat$u0), 0)
-    dat$C1 = dat$C0 + c(diff(dat$C0), 0)
-    dat$B0 = bubbleSat(dat$u0)
-    dat$B1 = bubbleSat(dat$u1)
-    dat$kw0 = kw('O2', dat$T0, dat$u0, dat$S0)
-    dat$kw1 = kw('O2', dat$T1, dat$u1, dat$S1)
-    dat$h1 = dat$h0 + c(diff(dat$h0), 0)
-    if("hMin" %in% colnames(x)){
-        dat$hMin0 = x$hMin
-        dat$hMin1 = x$hMin + c(diff(x$hMin), 0)
-        dat$hMax0 = x$hMax
-        dat$hMax1 = x$hMax + c(diff(x$hMax), 0)
-    }
-    dat$Pslp1 = dat$Pslp0 + c(diff(dat$Pslp0), 0)
-    dat$timePeriod = c(diff(as.numeric(dat$dateTime)), 0)
-    return(dat[-nrow(dat),])
-}
-
 #' O2 NCP
 #'
 #' calculate NCP based on O2 observations
@@ -40,8 +8,8 @@ transform_data <- function(x){
 #' @details TODO
 #' @param dat data frame matching the format outlined in XXX
 #' @param entrainment if True (default) calculate NCP with entrainment, Cb0 and Cb1 must be supplied
-#' @param kw.method character string passed to kw, default is 'WA09'
-#' @return a vector of NCP in uMol/l
+#' @param kw_method character string passed to kw, default is 'WA09'
+#' @return a vector of NCP in uMol/l per m-2 per supplied time interval
 #' @export
 O2NCP <- function(dat, entrainment = T, kw_method = 'WA09'){
     #subfunctions
@@ -71,12 +39,24 @@ O2NCP <- function(dat, entrainment = T, kw_method = 'WA09'){
     Q1.f <- function(tx){
         Gtx(tx) * exp(Rt(tx))
     }
+    calc_dhdt <- function(h0, h1, times, entrainment){
+        # calculate rate of change in mixed layer depth over time
+        # returns 0 for negative change
+        dhdt = (h1-h0)/max(times)
+        if(entrainment & dhdt > 0){
+            print('Entraining')
+            return(dhdt)
+        }
+        else{
+            return(0)
+        }
+    }
 
     ncp = vector()
     for(i in 1:nrow(dat)){
     # variable interpolators, can't be vectorised
     ti = dat[i,]$timePeriod
-    dhdt = 0
+    dhdt = calc_dhdt(dat[i,]$h0, dat[i,]$h1, ti, entrainment)
         mld  = approxfun(c(0, ti), c(dat[i,]$h0, dat[i,]$h1))
         temp = approxfun(c(0, ti), c(dat[i,]$T0, dat[i,]$T1))
         sal  = approxfun(c(0, ti), c(dat[i,]$S0, dat[i,]$S1))
@@ -89,7 +69,7 @@ O2NCP <- function(dat, entrainment = T, kw_method = 'WA09'){
         # calculate NCP (J)
 
     J = (dat[i,]$C1 * exp(Rt(ti)) - dat[i,]$C0 - Q1) / Q2
-    ncp = c(ncp, J * ti) # return as uMol per supplied time
+    ncp = c(ncp, J * ti) # return as uMol per supplied time per unit area
     }
     return(ncp)
 }
