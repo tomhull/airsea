87852c0b8b0f513f06a07b14fe800a449bc258ea
Tom Hull <betascoo8@gmail.com>
2015-08-12 22:05:49 +0100

    pressure scaling should be 1 ATM not average SLP!

--- R/ncp.R
+++ R/ncp.R
@@ -128,7 +128,7 @@ O2NCP.mean <- function(dat, asVolume = F, kw_method = 'WA13'){
              k = (kw('O2', T0, u0, S0) + kw('O2', T1, u1, S1))/2
              k = k + ((k / 100) * kw_error) # apply kw error
              h = (h0 + h1)/2
-             Prs = ((Pslp0 + Pslp1)/2) / 1013.25  # surface pressure scaling
+             Prs = ((Pslp0 + Pslp1)/2) / 1000  # surface pressure scaling
              B = (bubbleSat('O2', u0) + bubbleSat('O2', u1))/2
              B = B + ((B / 100) * B_error) # apply bubble error
              S = (Csat(T0, S0) + Csat(T1, S1))/2
--- tests/testthat/test_ncp.R
+++ tests/testthat/test_ncp.R
@@ -4,5 +4,5 @@ context("ncp")
 dat = data.frame(timePeriod = 90000, C0 = 282, C1 = 292, T0 = 10, T1 = 10.5, S0 = 35, S1 = 34.5, h0 = 25, h1 = 45, Pslp0 = 1000, Pslp1 = 998, u0 = 7, u1 = 5, Cb0 = 282, Cb1 = 282, entrainment = T)
 
 test_that("O2NCP.mean returns expected values", {
-              expect_equal(O2NCP.mean(dat), 479.1553, tolerance = 0.00001)
+              expect_equal(O2NCP.mean(dat), 471.6884, tolerance = 0.00001)
 })
