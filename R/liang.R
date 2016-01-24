05d9d83f412489efa6029f4da22b58ddf0231fc7
Tom Hull <betascoo8@gmail.com>
2016-01-24 16:00:06 +0000

    added ar, some refactoring

--- R/liang.R
+++ R/liang.R
@@ -1,5 +1,6 @@
 # implementation of liang 2013 piston and bubble stuff
 # using liang matlab names where possible
+require(airsea)
 
 solub <- function(T, S, igas){
   
@@ -20,8 +21,15 @@ solub <- function(T, S, igas){
   
   # Oxygen
   if(igas == 1){
+      # soly coefs taken from table.2 Weiss (volumetric solubilty constants)
     soly = -173.4292 + 249.6339 / t + 143.3483 * log(t) - 21.8492 * t + S * (-0.033096 + 0.014259 * t -0.0017 * t^2)
-    return( exp(soly) / (22.4*101325*0.20946) )
+    return( exp(soly) / (22.4*101325*0.20946) ) # 22.4 * mean sea level pressure (Pa) * air mole fraction
+  }
+  # Ar
+  if(igas == 4){
+      # soly coefs taken from table.2 Weiss (volumetric solubilty constants)
+    soly = -173.5146 + 245.4510 / t + 141.8222 * log(t) - 21.8020 * t + S * (-0.034474 + 0.014934 * t -0.0017729 * t^2);
+    return( exp(soly) / (22.4*101325*0.00934) )
   }
   # CO2
   if(igas==3){
@@ -29,7 +37,7 @@ solub <- function(T, S, igas){
     return( exp(soly) * 1e3 / 101325 )
   }
   
-  # TODO remaining gasses
+  # this is equiv to martin KH when converted to SI
 }
 
 solubility <- function(x, gasname){
@@ -38,8 +46,10 @@ solubility <- function(x, gasname){
   # sol = solubility in mole/m-3/atm
   # al = dimensionless solubility
   # sc schmidt number
-  r1 = 8.314
+  r1 = 8.314 # gas constant
   tsx=(x + 273.16) / 100 # I don't know why he changes is nomeclature here, same as t
+  ts = x + 273.16
+  print(paste("solubility called", gasname, x))
   
   if(gasname == 'CO2'){
     a = c(-60.2409, 93.4157, 23.3585)
@@ -49,64 +59,74 @@ solubility <- function(x, gasname){
     sol = exp(s) * 1000
   }
   if(gasname == 'O2'){
-    a = c(-58.3877, 85.8079, 23.8439)
-    b = c(-0.034892,  0.016241, -0.0020114)
-    a0 = c(1953.4, 128.0, 3.9918, 0.050091);
-    sol = solub(x, 35, 1) * 101325 * 0.20946
+    a = c(-58.3877, 85.8079, 23.8439) # these are the bunsen solubility constants from weiss
+    b = c(-0.034892,  0.016241, -0.0020114) # as above
+    a0 = c(1953.4, 128.0, 3.9918, 0.050091)
+    sol = solub(x, 35, 1) * 101325 * 0.20946 # convert to atm and * mole fraction
   }
-  al = sol / 1E5 * r1 * tsx * 100
+  if(gasname == 'Ar'){
+    a = c(-55.6578, 82.0262, 22.5929) # these are the bunsen solubility constants from weiss
+    b = c(-0.036267,  0.078374, -0.0033120) # as above
+    a0 = c(1909.1, 125.09, 3.9012, 0.048953)
+    sol = solub(x, 35, 1) * 101325 * 0.00934 # convert to atm and then?
+  }
+  
+  al = sol / 1E5 * r1 * ts # convert Hcp to ?dimensionless
   sc = a0[1] - a0[2] * x + a0[3] * x^2 - a0[4] * x^3
   return(c(sol, al, sc))
 }
 
-piston_velocity_L12 <- function(u10, sst, gasname){
+piston_velocity_L12 <- function(u10, sst, sal = 35, gasname){
   
+  print(paste("piston called", gasname, u10, sst, sal))
   y  = solubility(sst, gasname) # assumes 35 salinity!
   
-  alc = y[2]
-  Sc = y[3]
+  alc = y[2] # dimentionless henry's law solubility
+  Sc = y[3] # water side schmidtt
+  # Sc = Sch(gasname, sst, sal)
   
-  cd10 = dragCoef(u10)
+  cd10 = dragCoef(u10) # drag coeficient
 
   usr = sqrt(cd10) * u10
              
-  lam = 13.3
-  A = 1.3
-  phi = 1
-  rhow = 1022
-  rhoa = 1.0
-  hw = lam / A / phi
-  scwc = Sc
+  lam = 13.3 # 
+  A = 1.3 # fairall2011 empirical constant
+  phi = 1 # surface bouyancey flux enhancement compensation
+  rhow = 1022 # water density
+  rhoa = 1.0 # atmospheric density
+  hw = lam / A / phi # ?incorrect, fairall defines this as hw = 13.3 / (A * phi)
+  kappa = 0.4 # from Fairall2011 eq 3
   tkt = 0.01
+  scac = 0.9  # air-side schmidt number, why this isn't calculated i don't know
+  # Scac = Sc_air(gasname, sst)  # air-side schmidt number
 
-  rwo = sqrt(rhow / rhoa) * (hw * sqrt(scwc) + (log(0.5 / tkt) / 0.4))
+    # water side resistance to transfer (due to molecular turbulent processes)
+    # from fairall2011 eq13 sort of
+  rw = sqrt(rhow / rhoa) * (hw * sqrt(Sc) + (log(0.5 / tkt) / kappa))
    
-  ha = lam
-  scac = 0.9  # %air-side schmidt number
    
-  ra = ha * sqrt(scac) + 1 / sqrt(cd10) - 5 + 0.5 * log(scac) / 0.4 # %air side
+    # this is sort of fairall2011 eq 15
+  ra = lam * sqrt(scac) + 1 / sqrt(cd10) - 5 + 0.5 * log(scac) / kappa # air side resistance
      
-  usrw = usr / sqrt(1000)
+  usrw = usr / sqrt(1000) # frictional velocty ? doesn't use the definition in the paper
   #   
-  kbb = 1.98e6 * usrw^(2.76) / ((scwc / 660)^(2/3)) / 100 / 3600
+  kbb = 1.98e6 * usrw^(2.76) / ((Sc / 660)^(2/3)) / 100 / 3600
   #   
-  rw = rwo
-  #   
-  kw = 1 / (1 / (usr /rw / sqrt(scwc / 660) + kbb) + 1 / (usr / (ra * alc)))
+  kw = 1 / (1 / (usr /rw / sqrt(Sc / 660) + kbb) + 1 / (usr / (ra * alc)))
   return(kw)
 }
 
 gasex_overpressure_L12 <- function(u10, sst, gasname){
   
-  k0 = piston_velocity_L12(u10, sst, gasname)
+  k0 = piston_velocity_L12(u10, sst, gasname = gasname)
   
   cd10 = dragCoef(u10)
   
-  usr = sqrt(cd10) * u10 / sqrt(1000)
+  usr = sqrt(cd10) * u10 / sqrt(1000) # 
   
   y  = solubility(sst, gasname)
-  sol=y[1]
-  scwc=y[3]
+  sol=y[1] # henrys law volumetric solubility from weiss
+  scwc=y[3] # schmidt number
   
   sigmap = 1.5244 * usr^(1.06)
   finj = 5.56 * usr^(3.86)
