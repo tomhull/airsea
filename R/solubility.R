afd08eb1484c90db0ea1ef962f18eb3f282e119a
Tom Hull <tom.hull@cefas.co.uk>
2020-05-17 21:52:52 +0100

    added options for Csat

--- R/solubility.R
+++ R/solubility.R
@@ -1,17 +1,35 @@
 #' Equilibrium Oxygen saturation concentration
 #'
 #' Calculates oxygen saturation concentration in equilibrium with the atmosphere
-#' as per Garcia & Gordon, 1992 (Benson & Kraus data)
-#' @details TODO
+#' as per Garcia & Gordon, 1992
+#'
+#' conversions via SCOR WG 142
+#'
 #' @param temp numeric vector of water temperature in degrees Celsius
 #' @param salinity numeric vector of salinity (PSU)
+#' @param unit "molm" for mmol m-3 (default), "mgl" for mg l-1 or "molkg" for umol kg-1.
 #' @return vector of saturation concentration in mmol m-3
 #' @keywords oxygen
 #' @examples
-#' Csat(10, 35)  # saturation concentration at 10 degrees and 35 salinity
+#' oxygen.sat(10, 35)  # saturation concentration at 10 degrees and 35 salinity
 #' @export
-Csat <- function(temp, salinity){
-    # Coefficents
+Csat <- function(temp, salinity, unit = "molm"){
+  
+  if(unit == "molkg"){
+    # umol kg coefficents
+    A0 = 5.80871;
+    A1 = 3.20291;
+    A2 = 4.17887;
+    A3 = 5.10006;
+    A4 = -9.86643-2;
+    A5 = 3.80369;
+    B0 = -7.01577e-3;
+    B1 = -7.70028e-3;
+    B2 = -1.13864e-2;
+    B3 = -9.51519e-3;
+    C0 = -2.75915e-7;
+  }else{
+    # cm3 dm-3 coefficents (ml/l)
     A0 = 2.00907
     A1 = 3.22014
     A2 = 4.05010
@@ -23,16 +41,105 @@ Csat <- function(temp, salinity){
     B2 = -0.0103410
     B3 = -0.00817083
     C0 = -4.88682E-07
-
-    Ts = log((298.15-temp)/(273.15+temp))
-
-    O2.sat = A0+(A1*Ts)+(A2*Ts^2)+
+  }
+  Ts = log((298.15-temp)/(273.15+temp))
+  
+  O2.sat = A0+(A1*Ts)+(A2*Ts^2)+
     (A3*Ts^3)+(A4*Ts^4)+(A5*Ts^5)+
     salinity*(B0+(B1*Ts)+(B2*Ts^2)+(B3*Ts^3))+
     (C0*salinity^2)
+  
+  # molar volume of O2 of 22,39 1.6 cm3 mol-1
+  
+  if(unit == "molm"){
+    return(exp(O2.sat) * 44.6596)     # convert ml/l to mmol m-3  as per SCOR WG 142
+  }
+  if(unit == "mll"){
+    return(exp(O2.sat)) # no conversion
+  }
+  if(unit == "mgl"){
+    return(exp(O2.sat) / 0.699745)     # convert ml/l to mg/l
+  }
+  if(unit == "molkg"){
+    return(exp(O2.sat)) # no conversion
+  }
+  
+  # 1 μmol O2 = .022391 ml at sea surface pressure
+  # 1 mg/l = 22.391 ml/31.998 = 0.699745 ml/l
+  
+  else{
+    stop("unit not recognised")
+  }
+}
 
-    return((exp(O2.sat))* 44.6608)     # output in uMol/L
-    # TODO refer to solubility for non - "O2"
+#' Equilibrium Oxygen saturation concentration (combined fit)
+#'
+#' Calculates oxygen saturation concentration in equilibrium with the atmosphere
+#' as per Garcia & Gordon, 1992, using the combined fit, which is not recomended.
+#'
+#' @param temp numeric vector of water temperature in degrees Celsius
+#' @param salinity numeric vector of salinity (PSU)
+#' @param unit "molm" for mmol m-3 (default), "mgl" for mg l-1 or "molkg" for umol kg-1.
+#' @return vector of saturation concentration in mmol m-3
+#' @keywords oxygen
+#' @examples
+Csat.combined <- function(temp, salinity, unit = "molm"){
+  
+  if(unit == "molkg"){
+    # umol kg coefficents
+    A0 = 5.80818
+    A1 = 3.20684
+    A2 = 4.11890
+    A3 = 4.93845
+    A4 = 1.01567
+    A5 = 1.41575
+    B0 = -7.01211e-3
+    B1 = -7.25958e-3
+    B2 = -7.93334e-3
+    B3 = -5.54491e-3
+    C0 = -1.32412e-7
+  }else{
+    # cm3 dm-3 coefficents (ml/l)
+    A0 = 2.00856
+    A1 = 3.22400
+    A2 = 3.99063
+    A3 = 4.80299
+    A4 = 9.78188e-1
+    A5 = 1.71069
+    B0 = -6.24097e-3
+    B1 = -6.93498e-3
+    B2 = -6.90358e-3
+    B3 = -4.29155e-3
+    C0 = -3.11680e-7
+  }
+  Ts = log((298.15-temp)/(273.15+temp))
+  
+  O2.sat = A0+(A1*Ts)+(A2*Ts^2)+
+    (A3*Ts^3)+(A4*Ts^4)+(A5*Ts^5)+
+    salinity*(B0+(B1*Ts)+(B2*Ts^2)+(B3*Ts^3))+
+    (C0*salinity^2)
+  
+  # molar volume of O2 of 22,39 1.6 cm3 mol-1
+  
+  if(unit == "molm"){
+    return(exp(O2.sat) * 44.6596)     # convert ml/l to mmol m-3  as per SCOR WG 142
+  }
+  if(unit == "mll"){
+    return(exp(O2.sat)) # no conversion
+  }
+  if(unit == "mgl"){
+    return(exp(O2.sat) / 0.699745)     # convert ml/l to mg/l
+  }
+  if(unit == "molkg"){
+    return(exp(O2.sat)) # no conversion
+  }
+  
+  # 1 μmol O2 = .022391 ml at sea surface pressure
+  # 1 mg/l = 22.391 ml/31.998 = 0.699745 ml/l
+  
+  else{
+    stop("unit not recognised")
+  }
 }
 
 
